e9be66e8eae34bd0b5a26bf94311fd8045ab83bd
lllilyk <ym0402kkk@naver.com>
2023-02-21 23:43:26 +0900

    programmershome

---
+++ src/codetest/programmers/Solution120806.java
@@ -0,0 +1,13 @@
+package codetest.programmers;
+
+public class Solution120806 {
+	public static void main(String[] args) {
+		int num1 = 7;
+		int num2 = 3;
+		
+		double result = 0;
+		result = ((double)num1 / num2) * 1000;
+		int result1 = (int)result;
+		System.out.println(result1);
+	}
+}
--- src/codetest/programmers/Solution120822.java
+++ src/codetest/programmers/Solution120822.java
@@ -3,15 +3,23 @@ package codetest.programmers;
 public class Solution120822 {
 	public static void main(String[] args) {
 		
-//		
-//		for(int i = 1; i <=str.length() ; i++) {
-//			System.out.print(str.charAt(str.length() -i));
-//		}
-//		
-		String str = "bread";
-		for(int i = str.length()-1; i >= 0 ; i--) {
-			System.out.print(str.charAt(str.length()));
-		}
-	}
+		/*String str = "bread";
+		for(int i = 1; i <=str.length() ; i++) {
+			System.out.print(str.charAt(str.length() -i));
+		}*/
 
+		String str = "bread";
+//		System.out.println(str.length());
+//		System.out.println(str.length()-1);
+//		System.out.println(str.length()-2);
+//		System.out.println(str.length()-3);
+//		System.out.println(str.length()-4);
+		
+//		for(int i = str.length(); i >= 1 ; i--) {
+			for(int j = str.length()-1; j >= 0; j--) {
+			System.out.print(str.substring(j,j+1));
+		}
+//	}
+
+	}
 }
